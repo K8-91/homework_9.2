21ae7abbb32a236d208733e619ce969766d254ed
Kasia Raczkier <katarzyna.raczkier@gmail.com>
2020-08-05 21:10:42 +0200

    Second review

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import json
 import csv
+from flask import Flask
+from flask import request, redirect
+from flask import render_template
 
 with open('bank.json', "r") as jsonfile:
     bank_py = json.load(jsonfile)
 
-rates_to_CSV = (bank_py['rates'])
+rates_to_CSV = bank_py['rates']
+
+currency_rates = {}
+
+for item in rates_to_CSV:
+    currency_rates[item['code']] = item
+
 
 
 rates_keys = rates_to_CSV[0].keys()
@@ -12,10 +21,6 @@ with open("bank.csv", 'w', newline='') as output_file:
     dict_writer = csv.DictWriter(output_file, delimiter =';', fieldnames= rates_keys)
     dict_writer.writeheader()
     dict_writer.writerows((rates_to_CSV))
-
-from flask import Flask
-from flask import request, redirect
-from flask import render_template
 
 
 app = Flask(__name__)
@@ -26,13 +31,14 @@ def currency():
         return render_template("currency.html")
     if request.method == 'POST':
         data = request.form
-        currency = data.get['currency']
-        quantity = float(data.get['quantity'])
-        for item in rates_to_CSV:
-            if currency == item['code']:
-                result = quantity * item['ask']
-                return result
+        currency = data['currency']
+        quantity = float(data['quantity'])
+        if currency_rates.get(currency):
+            result = currency_rates[currency]['ask'] * quantity
+            return render_template("currency.html", result=result)
+
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run()
+
 
